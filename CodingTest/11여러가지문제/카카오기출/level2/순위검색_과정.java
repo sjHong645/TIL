9752ad7a85cd1aa8360ae164b3532b23a4286016
sponbob-pat <64796257+sponbob-pat@users.noreply.github.com>
2022-07-07 14:06:58 +0900

    순위검색 임시6
    5는 생략

--- CodingTest/11여러가지문제/카카오기출/level2/순위검색_과정.java
+++ CodingTest/11여러가지문제/카카오기출/level2/순위검색_과정.java
@@ -1,197 +1,142 @@
-// 트리는 어떤가...
-// 맨 위는 언어, 그 다음 층은 직군, 그 다음 층은 경력, 그 다음 층은 음식, 그 다음 층은 점수 
+// 참여 개발 언어 - cpp, java, python : 1, 2, 3
+// 지원 직군 - backend, frontend : 1, 2
+// 경력 - junior, senior : 1, 2
+// 음식 - chicken, pizza : 1, 2
+// ex) 2112 - java로 코테를 보고 backend에 지원. 경력은 junior, 음식은 chicken
 
-// 문제의 조건을 보니까, 직군, 경력, 음식 모두 2개밖에 없다. = 이진트리로 만들기 딱 좋다. 
-// 루트는 cpp, java, python 3개로 만들고
+// 코딩테스트 점수 - List로 저장
 
-// 직군 - left : backend, right : frontend
-// 경력 - left : junior, right : senior
-// 소울푸드 - left : chicken, right : pizza
-// 점수 - 입력받는 자료형은 String이다.
-
-// 범위가 정해져있으니까 각각의 case들을 배열로 표현할 수 있지 않을까.
-
-import java.util.List; 
+import java.util.List;
 import java.util.ArrayList; 
+
+import java.util.Map;
+import java.util.HashMap;
 
 import java.util.StringTokenizer;
 
-import java.util.Arrays;
-
 class Solution {
-    
-    /*static List<Integer>[] cpplist;
-    static List<Integer>[] javalist;
-    static List<Integer>[] pylist; */
-    static List<Integer>[] list; 
-    
-    static StringTokenizer st; 
-    
     public int[] solution(String[] info, String[] query) {
-        int[] answer = {};
+        int[] answer = new int[query.length];
         
-        // 정보 저장
-        /*cpplist = new ArrayList[8];
-        javalist = new ArrayList[8];
-        pylist = new ArrayList[8]; */
+        // Map<Integer, List<Integer>> inform = new HashMap<>();
+        Map<Integer, List<Integer>> inform = new HashMap<>();
         
-        list = new ArrayList[24]; // 0 ~ 7 : cpp, 8 ~ 15 : java, 16 ~ 23 : python
-         
-        for(int i = 0; i < 24; i++) {
-            
-            /*cpplist[i] = new ArrayList<>();
-            javalist[i] = new ArrayList<>();
-            pylist[i] = new ArrayList<>(); */
-            list[i] = new ArrayList<>();
-            
-        }
+        StringTokenizer st; 
         
-        String lang;
+        // info에 있는 내용 처리
+        int keyNum = 0; int score = 0;
         for(int i = 0; i < info.length; i++) { 
-            
             st = new StringTokenizer(info[i]);
             
-            lang = st.nextToken();
-            /* if(lang.equals("cpp")) { setData(cpplist, st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken());}
+            // 언어, 직군, 경력, 음식
+            keyNum = makeNumber(st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken());
             
-            else if(lang.equals("java")) { setData(javalist, st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken());}
+            // 코딩테스트 점수 
+            score = Integer.parseInt(st.nextToken());
             
-            else if(lang.equals("python")) { setData(pylist, st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken());} */
-            setData(list, lang, st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()); 
+            // 아직 keyNum에 대해 정의된 게 없을 때
+            if(!inform.containsKey(keyNum)) { 
+                inform.put(keyNum, new ArrayList<>());
+                inform.get(keyNum).add(score);
+            }
             
+            // 정의된 게 있을 때
+            else { 
+                inform.get(keyNum).add(score);
+            }
             
         }
+                
+        // query 
+        String lang, group, career, food; int std;
+        int queryNum = 0; 
         
-        // 쿼리 
-        String group, exp, food, score;
-        
-        boolean[] visited = new boolean[8]; // true - 방문해야 할 인덱스
-        
+        int first, second, third, fourth;
         for(int i = 0; i < query.length; i++) { 
             
-            st = new StringTokenizer(query[i]); 
-            lang = st.nextToken(); st.nextToken(); // 언어 - cpp, java, python 
-            group = st.nextToken(); st.nextToken(); // 직군
-            exp = st.nextToken(); st.nextToken(); // 경력
-            food = st.nextToken(); // 음식
+            st = new StringTokenizer(query[i]);
             
-            score = st.nextToken(); // 점수
+            lang = st.nextToken(); st.nextToken();
+            group = st.nextToken(); st.nextToken();
+            career = st.nextToken(); st.nextToken();
+            food = st.nextToken(); 
             
+            queryNum = makeNumber(lang, group, career, food);
             
-            qur(list, lang, group, exp, food, score); 
+            std = Integer.parseInt(st.nextToken());
+            
+            // 해당하는 조건에 정확히 맞는 사람이 있을 때
+            if(inform.containsKey(queryNum)) {
+                
+                for(int e : inform.get(queryNum)) {
+                    if(e >= std) answer[i]++;
+                }
+                
+            }
+            
+            else { 
+                String queryString = Integer.toString(queryNum);
+                
+                // queryNum의 첫 번째 자리
+                first = queryString.charAt(0) - '0';
+                // queryNum의 두 번째 자리
+                second = queryString.charAt(1) - '0';
+                // queryNum의 세 번째 자리
+                third = queryString.charAt(2) - '0';
+                // queryNum의 네 번째 자리
+                fourth = queryString.charAt(3) - '0';
+                
+                // 4를 포함하지 않은 경우
+                if(first != 4 && second != 4 && third != 4 && fourth != 4) answer[i] = 0;
+                
+                // 4를 포함한 경우 
+                else {
+                    for(int k : inform.keySet()) {
+                    
+                    // 
+                    
+                    }
+                }
+                
+                
+                
+                
+                
+                
+            }
             
         }
-        
         
         return answer;
     }
     
-    static void qur(List<Integer>[] list, String lang, String group, String exp, String food, String score) { 
+    static int makeNumber(String lang, String group, String career, String food) { 
         
-        // true : 해당 값을 사용함
-        boolean[] button = new boolean[24];
+        int number = 0; 
         
-        // lang - 언어
-        if(lang.equals("cpp")) { Arrays.fill(button, 0, 8, true); }
+        // 언어
+        if(lang.equals("java")) number += 1000; 
+        else if(lang.equals("cpp")) number += 2000; 
+        else if(lang.equals("python")) number += 3000;
+        else number += 4000; 
         
-        else if(lang.equals("java")) { Arrays.fill(button, 8, 16, true); }
+        // 직군
+        if(group.equals("backend")) number += 100; 
+        else if(group.equals("frontend")) number += 200;        
+        else number += 400;
         
-        else if(lang.equals("python")) { Arrays.fill(button, 17, 23, true); }
+        // 경력 
+        if(career.equals("junior")) number += 10; 
+        else if(career.equals("senior")) number += 20;  
+        else number += 40;
         
-        // group - 직군
-        // 직군에 해당하지 않는 부분을 false로 만듦
-        if(group.equals("backend")) { 
-            Arrays.fill(button, 4, 8, false); 
-            Arrays.fill(button, 12, 16, false); 
-            Arrays.fill(button, 20, 24, false);
-        }
-        
-        else if(group.equals("frontend")) {
-            Arrays.fill(button, 0, 4, false); 
-            Arrays.fill(button, 8, 12, false); 
-            Arrays.fill(button, 16, 20, false);
-        }
-        
-        // exp - 경력
-        if(group.equals("junior")) { 
-        
-        }
-        
-        else if(group.equals("senior")) {
-
-        }
-        
-        // food - 음식
-        if(group.equals("chicken")) { 
-            for(int i = 0; i < list.length; i++) {
+        // 음식 
+        if(food.equals("chicken")) number += 1; 
+        else if(food.equals("pizza")) number += 2;        
+        else number += 4;
                 
-                if(i % 2 == 1) button[i] = false;
-                    
-            }
-        }
-        
-        else if(group.equals("pizza")) {
-            for(int i = 0; i < list.length; i++) {
-                
-                if(i % 2 == 0) button[i] = false;
-                    
-            }
-        }        
-        
-        
-        // score - 점수
-        
-    }
-    
-    static void setData(List<Integer>[] list, String lang, String group, String exp, String food, String score) { 
-        int mul = 0;
-        if(lang.equals("cpp")) mul = 0; 
-        
-        else if(lang.equals("java")) mul = 1; 
-        
-        else if(lang.equals("python")) mul = 2; 
-        
-        if(group.equals("backend") && exp.equals("junior") && food.equals("chicken")) {
             
-            list[0 + mul * 8].add(Integer.parseInt(score)); 
-        }
-        
-        else if(group.equals("backend") && exp.equals("junior") && food.equals("pizza")) {
-            
-            list[1 + mul * 8].add(Integer.parseInt(score)); 
-        }
-        
-        else if(group.equals("backend") && exp.equals("senior") && food.equals("chicken")) {
-            
-            list[2 + mul * 8].add(Integer.parseInt(score)); 
-        }
-        
-        else if(group.equals("backend") && exp.equals("senior") && food.equals("pizza")) {
-            
-            list[3 + mul * 8].add(Integer.parseInt(score)); 
-        }
-        
-        else if(group.equals("frontend") && exp.equals("junior") && food.equals("chicken")) {
-            
-            list[4 + mul * 8].add(Integer.parseInt(score)); 
-        }
-        
-        else if(group.equals("frontend") && exp.equals("junior") && food.equals("pizza")) {
-            
-            list[5 + mul * 8].add(Integer.parseInt(score)); 
-        }
-        
-        else if(group.equals("frontend") && exp.equals("senior") && food.equals("chicken")) {
-            
-            list[6 + mul * 8].add(Integer.parseInt(score)); 
-        }
-        
-        else if(group.equals("frontend") && exp.equals("senior") && food.equals("pizza")) {
-            
-            list[7 + mul * 8].add(Integer.parseInt(score)); 
-        }
-        
-        
+        return number;
         
     }
 }
